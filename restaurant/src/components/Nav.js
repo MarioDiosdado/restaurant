0f148a3dedabb2d1232c7dcb9127288e6bf485e0
Mario Diosdado <diosdado.mario@gmail.com>
2021-03-08 12:47:47 -0600

    Navbar menu animation

--- restaurant/src/components/Nav.js
+++ restaurant/src/components/Nav.js
@@ -1,11 +1,12 @@
 import React from "react";
-// import logo from "../../public/images/logo.png"
+import "../style/nav.css"
+
 const styles = {
     nav: {
         background: "#BF0000",
         color: "white",
         padding: "0px",
-        height: "80px"
+        height: "80px",
     }
 }
 
@@ -13,13 +14,13 @@ function navBar() {
     return <div>
         <nav className="navbar navbar-expand-lg navbar-dark" style={styles.nav}>
             <a className="navbar-brand" href="/">
-                <img src={process.env.PUBLIC_URL + '/images/logo.png'} width="207" height="80" class="d-inline-block align-top" alt=""></img></a>
+                <img src={process.env.PUBLIC_URL + '/images/logonew.jpg'} width="207" height="80" class="d-inline-block align-top" alt=""></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="/">MENU <span className="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup" style={styles.navItems}>
+                <div className="navbar-nav mynav navItems">
+                    <a className="nav-item nav-link" href="/">MENU <span className="sr-only">(current)</span></a>
                     <a className="nav-item nav-link" href="/">LOCATIONS</a>
                     <a className="nav-item nav-link" href="/">CATERING</a>
                     <a className="nav-item nav-link" href="/">MERCH</a>
@@ -29,7 +30,7 @@ function navBar() {
             </div>
             <form class="form-inline my-2 my-lg-0">
             <a href="/">
-                <img src={process.env.PUBLIC_URL + '/images/order.png'} width="207" height="80" class="d-inline-block align-top" alt=""></img>
+                <img src={process.env.PUBLIC_URL + '/images/order.jpg'} width="207" height="80" class="d-inline-block align-top" alt=""></img>
                 </a>
             </form>
         </nav>
