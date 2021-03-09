b11f8b3e89bdddb00761c5293f9c648c66afcca2
Mario Diosdado <diosdado.mario@gmail.com>
2021-03-08 20:05:15 -0600

    Worked on buttons

--- restaurant/src/components/Jumbo.js
+++ restaurant/src/components/Jumbo.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import "../style/nav.css"
 
 const styles = {
     box: {
         background: "#BF0000"
     },
     images: {
-        padding: 0
+        position: "relative",
+        padding: 0,
+        width: "100%",
+        height: "auto",
     },
     mail: {
         padding: "15px",
@@ -22,10 +25,16 @@ function Jumbo() {
         <div className="mainpage" style={styles.box}>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-8" style={styles.images}>
+                    <div className="col-md-8 col-sm-12" style={styles.images}>
                         <img src={process.env.PUBLIC_URL + '/images/main.jpg'} className="img-fluid" alt="Responsive imag"></img>
+                        <div className="img-overlay">
+                            <h1 className="month">STEAK OF THE MONTH</h1>
+                        </div>
+                        <div className="img-overlay2">
+                            <button type="button" className="btn btn-danger btn-lg btn-responsive">TRY IT NOW</button>
+                        </div>
                     </div>
-                    <div className="col" style={styles.images}>
+                    <div className="col-md-4 col-sm-12" style={styles.images}>
                         <div className="row">
                             <div className="col"><img src={process.env.PUBLIC_URL + '/images/main2.jpg'} className="img-fluid" alt="Responsive imag"></img></div>
                         </div>
