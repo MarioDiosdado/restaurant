3dacad524530ea44917654af15060897cf02cc6d
Austin Lambert <atlambert11@gmail.com>
2021-03-14 15:43:51 -0500

    Established Menu page, and added MenuTypes component

---
+++ restaurant/src/components/MenuType.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MenuType() {
+    return (
+        <div className="card" style={{width: "18rem"}}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <p className="card-text">MENU TYPE HERE</p>
+            </div>
+        </div>
+    )
+}
+
+export default MenuType;
---
+++ restaurant/src/pages/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Menu.css"
+import MenuType from "../../components/MenuType";
+
+function Menu() {
+
+    return (
+        <div id="background">
+            <h1 id="header">Rare/Medium Rare Or Get Out</h1>
+        </div>
+    )
+}
+
+export default Menu;
